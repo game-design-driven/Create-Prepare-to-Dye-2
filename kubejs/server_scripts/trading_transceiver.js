ca74c3f0329de26954ac0dd9156590afec3ad3b2
Yarden zamir <hrsidsad@gmail.com>
2025-12-31 21:09:26 +0200

    feat: make trading tranciever show closest border

--- kubejs/server_scripts/trading_transceiver.js
+++ kubejs/server_scripts/trading_transceiver.js
@@ -7,6 +7,28 @@ ItemEvents.rightClicked('ptdye:trading_transceiver', function(event) {
     if (!target || target.type !== 'block') return;
 
     var block = target.block;
+
+    // Prevent use outside owned territory (when territory enforcement is active)
+    if (global.ChunkTerritory && global.ChunkTerritory.chunks && Object.keys(global.ChunkTerritory.chunks).length > 0) {
+        var uuid = player.uuid.toString();
+        var pcx = player.chunkPosition().x;
+        var pcz = player.chunkPosition().z;
+        var ownsPlayerChunk = global.ChunkTerritory.owns(uuid, pcx, pcz);
+
+        var bcx = Math.floor(block.x / 16);
+        var bcz = Math.floor(block.z / 16);
+        var ownsTargetChunk = global.ChunkTerritory.owns(uuid, bcx, bcz);
+
+        if (!ownsPlayerChunk || !ownsTargetChunk) {
+            var bx = block.x + 0.5, by = block.y + 0.5, bz = block.z + 0.5;
+            event.server.runCommandSilent('particle angry_villager ' + bx + ' ' + by + ' ' + bz + ' 0.2 0.2 0.2 0 6 force ' + player.name.string);
+            event.server.runCommandSilent('playsound minecraft:block.note_block.bass master ' + player.name.string + ' ' + bx + ' ' + by + ' ' + bz + ' 0.6 0.7');
+            player.tell('You must be inside your territory to use the Trading Transceiver');
+            event.cancel();
+            return;
+        }
+    }
+
     var x = block.x + 0.5;
     var y = block.y + 50;
     var z = block.z + 0.5;
@@ -21,11 +43,11 @@ ItemEvents.rightClicked('ptdye:trading_transceiver', function(event) {
     // Place agreement after landing (delay for fall time)
     var playerName = player.name.string;
     var landX = block.x, landZ = block.z;
+    var itemstring = 'item replace block ' + landX + ' ' + landY + ' ' + landZ + ' container.0 with wares:delivery_agreement{title:\'"Chunk Purchase"\',requestedItems:[{id:"minecraft:stick",Count:1b}],paymentItems:[],deliveryTime:60,commands:{onDelivery:"territory add_adjacent ' + playerName + ' 60",onCompletion:"territory add_adjacent ' + playerName + ' 60"}}'
     event.server.scheduleInTicks(120, function() {
-        event.server.runCommandSilent(
-            'item replace block ' + landX + ' ' + landY + ' ' + landZ + ' container.0 with wares:delivery_agreement{title:\'"Chunk Purchase"\',requestedItems:[{id:"minecraft:stick",Count:1b}],paymentItems:[],deliveryTime:60,commands:{onDelivery:"territory add_adjacent ' + playerName + ' 60",onCompletion:"territory add_adjacent ' + playerName + ' 60"}}'
-        );
+        event.server.runCommand(itemstring);
     });
+    console.log(itemstring);
 
     // Sound effects
     event.server.runCommandSilent(
